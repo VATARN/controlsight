841569881fdc7373e989e7dfce8834741d6c82a3
Arnav Vats <arnav.vats@utexas.edu>
2024-04-25 04:37:36 -0500

    created Navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import Logo from "@/public/logo.webp";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+
+const NavBar = () => {
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues", label: "Issues" },
+  ];
+
+  const currentPath = usePathname();
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <Image src={Logo} alt="logo" className="h-10 w-10" />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map(({ href, label }) => (
+          <Link
+            key={href}
+            href={href}
+            className={classNames({
+              "text-zinc-900": href === currentPath,
+              "text-zinc-500": href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+          >
+            {label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
